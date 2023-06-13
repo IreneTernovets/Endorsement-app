b2832ac0d34b1ab84254f6b48fa51833626a1e50
Irene <terna.legend@gmail.com>
2023-06-13 13:38:18 +0300

    imported database methods, created html and js files

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://playground-1eb65-default-rtdb.europe-west1.firebasedatabase.app",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const endorsementInDB = ref(database, "Endorsemnts");
+
+const messageFieldEl = document.getElementById("message-input");
+const publishBtn = document.getElementById("publish-btn");
+const endorsementParagraphs = document.getElementById("endorsements");
+
+publishBtn.addEventListener("click", function () {
+  let inputValue = messageFieldEl.value;
+  push(endorsementInDB, inputValue);
+  endorsementParagraphs.innerHTML += `<p>${inputValue}</p>`;
+  messageFieldEl.value = "";
+});
