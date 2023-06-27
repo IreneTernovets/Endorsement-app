72814dacb7bc4373b32311224199e2e49d4d868f
Irene <terna.legend@gmail.com>
2023-06-27 18:58:49 +0300

    release of app

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
+  remove,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
@@ -21,6 +23,35 @@ const endorsementParagraphs = document.getElementById("endorsements");
 publishBtn.addEventListener("click", function () {
   let inputValue = messageFieldEl.value;
   push(endorsementInDB, inputValue);
-  endorsementParagraphs.innerHTML += `<p>${inputValue}</p>`;
-  messageFieldEl.value = "";
+  clearInputField();
 });
+
+onValue(endorsementInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+    endorsementParagraphs.innerHTML = "";
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      appendNewItem(currentItem);
+    }
+  } else {
+    endorsementParagraphs.innerHTML = "Start sharing your love...";
+  }
+});
+
+function clearInputField() {
+  messageFieldEl.value = "";
+}
+
+function appendNewItem(item) {
+  let itemID = item[0];
+  let itemValue = item[1];
+  let newEl = document.createElement("p");
+  newEl.textContent = itemValue;
+
+  newEl.addEventListener("dblclick", function () {
+    let endorsementExactLocationInDB = ref(database, `Endorsemnts/${itemID}`);
+    remove(endorsementExactLocationInDB);
+  });
+  endorsementParagraphs.append(newEl);
+}
